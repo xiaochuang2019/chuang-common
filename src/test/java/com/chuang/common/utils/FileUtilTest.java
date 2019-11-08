cc26efce740c4fac5a54ef34d219716ab9e6ab47
xiaochuang <805227385@qq.com>
2019-11-08 14:36:31 +0800

    添加文件处理工具包

---
+++ src/test/java/com/chuang/common/utils/FileUtilTest.java
@@ -0,0 +1,29 @@
+package com.chuang.common.utils;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class FileUtilTest {
+
+	@Test
+	public void testGetExtendName() {
+		String name = FileUtil.getExtendName("name.pptx");
+		System.out.println(name);
+	}
+
+	@Test
+	public void testGetTempDirectory() {
+		File file = FileUtil.getTempDirectory();
+		System.out.println(file);
+	}
+
+	@Test
+	public void testGetUserDirectory() {
+		File file = FileUtil.getUserDirectory();
+		System.out.println(file);
+	}
+
+}
