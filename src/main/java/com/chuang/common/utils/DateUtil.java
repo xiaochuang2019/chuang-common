4116855206289b4b1392a93f2427c9e778191110
xiaochuang <805227385@qq.com>
2019-11-08 14:21:34 +0800

    增加区间随机获取时间

--- src/main/java/com/chuang/common/utils/DateUtil.java
+++ src/main/java/com/chuang/common/utils/DateUtil.java
@@ -64,4 +64,11 @@ public class DateUtil {
 		 */
 		return c.getTime();
 	}
+	//生成指定范围内随机日期.如 2010年1月1日至今任意随机时间
+		public static Date randomDate(Date d1,Date d2) {
+			long l1= d1.getTime();
+			long l2=d2.getTime();
+			long l3=(long)((Math.random()*(l2-l1+1))+l1);
+			return new Date(l3);
+		}
 }
