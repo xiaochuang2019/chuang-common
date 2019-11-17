69c7ca8b2f4a6c502c6fd3ec0294744b95801225
xiaochuang <805227385@qq.com>
2019-11-17 18:40:19 +0800

    添加邮箱以及手机号验证

--- src/main/java/com/chuang/common/utils/StringUtil.java
+++ src/main/java/com/chuang/common/utils/StringUtil.java
@@ -70,7 +70,7 @@ public class StringUtil {
 			String name=firstNameString+chineseString;
 			return name;
 		}
-		
+		//手机号验证
 		public static boolean isPhone(String phone) {
 			String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
 		    if (phone.length() != 11) {
@@ -82,6 +82,7 @@ public class StringUtil {
 		        return isMatch;
 		    }
 		}
+		//邮箱验证
 		public static boolean isEmail(String email){  
 	        if (null==email || "".equals(email)){
 	        	return false; 
